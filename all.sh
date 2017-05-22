d84bdc4d33032cf869c0e6570f57f413cdd68430
Fonix Hardcastle <actualfriction@gmail.com>
2017-05-22 19:47:35 +0000

    all.sh Updated

--- all.sh
+++ all.sh
@@ -1,40 +1,31 @@
+#!/bin/sh
 echo "Running start all scripts! <3"
 
-cd /fs-data/backups/files/script-bank
+cd /home/backup-scripts
 
-./dh-io-1-backups.sh
-echo "Node Complete"
-sleep 1
-
-./dh-io-2-backups.sh
-echo "Node Complete"
-sleep 1
-
-./dh-io-3-backups.sh
-echo "Node Complete"
-sleep 1
-
-./dh-io-4-backups.sh
-echo "Node Complete"
-sleep 1
-
-./dh-io-5-backups.sh
-echo "Node Complete"
-sleep 1
-
-./dh-mh-1.sh
-echo "Node Complete"
-sleep 1
-
-./dh-mh-2.sh
-echo "Node Complete"
-sleep 1
-
+cd local-backups
 ./solder-backup.sh
-echo "Node Complete"
+cd ..
+echo "local-backup complete"
 sleep 1
+
+cd per-server-backups
+./*.sh
+cd ..
+echo "per server backups complete"
+sleep 1
+
+cd whole-server-backups
+./*.sh
+cd ..
+echo "whole backups complete"
+sleep 1
+
+cd whole-vm-backups
+./*.sh
+cd ..
+echo "whole vm backups complete"
+sleep 1
+
 
 echo "All files complete"
-
-
-
